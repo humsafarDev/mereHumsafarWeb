d1a679a66c17e59cf1cf7c85e3238d341f4f10fe
Sailendra Kumar <sailendraies@gmail.com>
2025-07-29 12:43:45 +0530

    setup project by sail

---
+++ src/Utils/axiosInstance.js
@@ -0,0 +1,78 @@
+
+import axios from 'axios';
+
+
+const axiosInstance = axios.create({
+  baseURL: '/api',
+});
+
+axiosInstance.defaults.headers.common['skip_zrok_interstitial'] = 'true';
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    console.error("Request error: ", error);
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if ((error.response?.status === 401 || error.response?.status === 403) && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      const refreshToken = localStorage.getItem('refreshToken');
+      if (refreshToken) {
+        try {
+          delete axiosInstance.defaults.headers.common['Authorization'];
+
+          const response = await axios.post(`/baseUrl/auth-service/api/v1/auth/user/refresh-token?refreshToken=${refreshToken}`,
+             {
+            headers: {
+              'skip_zrok_interstitial': 'true',
+            },
+          }
+          );
+          if(response?.data?.code == 200){
+            const newAccessToken = response.data?.body?.accessToken;
+            if (newAccessToken) {
+              localStorage.setItem('accessToken', newAccessToken);
+  
+              originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+  
+              return axiosInstance(originalRequest);
+            }
+          }else{
+            alert("Session expired. Please log in again.");
+            localStorage.removeItem("accessToken");
+            localStorage.removeItem("refreshToken");
+            window.location.href = '/login';
+          }
+
+        } catch (refreshError) {
+          alert("Session expired. Please log in again.");
+          localStorage.removeItem("accessToken");
+          localStorage.removeItem("refreshToken");
+          window.location.href = '/login';
+          return Promise.reject(refreshError);
+        }
+      } else {
+        console.error("No refresh token available. Redirecting to login.");
+        window.location.href = '/login';
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
---
+++ src/context/authContext.js
@@ -0,0 +1,105 @@
+import { create } from "zustand";
+import ErrorHandle from "../Components/Widgets/ErrorHandle";
+
+import { queryClient } from "../utils/queryClient";
+import axiosInstance from "../Utils/axiosInstance";
+import { verify } from "../api/authApi";
+
+const useAuthStore = create((set) => ({
+    user: null,
+    accessToken: localStorage.getItem("accessToken") || null,
+    refreshToken: localStorage.getItem("refreshToken") || null,
+    isAuthenticated: !!localStorage.getItem("accessToken"),
+    checkAuthOnReload: () => {
+        const accessToken = localStorage.getItem("accessToken");
+        const refreshToken = localStorage.getItem("refreshToken");
+
+        if (accessToken && refreshToken) {
+            set({ accessToken, refreshToken, isAuthenticated: true });
+        } else {
+            localStorage.removeItem("accessToken");
+            localStorage.removeItem("refreshToken");
+            set({ accessToken: null, refreshToken: null, isAuthenticated: false });
+        }
+    },
+    login: async (payload) => {
+        try {
+            const response = await axiosInstance.post('/auth/login', payload);
+
+            if (response.data.status === "success") {
+                // const { accessToken, refreshToken } = response.data.body;
+                // set({ accessToken, refreshToken, isAuthenticated: true  });
+                // localStorage.setItem("accessToken", accessToken);
+                // localStorage.setItem("refreshToken", refreshToken);
+            }
+
+            return response;
+        } catch (error) {
+            console.log("Login error:", error);
+            throw error;
+        }
+    },
+
+    verify: async (payload) => {
+        try {
+            const response = await verify(payload);
+            if (response?.data?.code === 200 || response?.data?.code === 201) {
+             
+                const accessToken = response.data.body;
+                const refreshToken = response.data.body;
+                set({ accessToken, refreshToken, isAuthenticated: true });
+                localStorage.setItem("accessToken", accessToken);
+                localStorage.setItem("refreshToken", refreshToken);
+            } else {
+                alert(response?.data?.message || "Verification failed");
+            }
+            return response;
+        } catch (error) {
+            ErrorHandle(error);
+            throw error;
+        }
+    },
+
+    signup: async (payload) => {
+        try {
+            const response = await axiosInstance.post('/auth/signup', payload);
+            return response;
+        } catch (error) {
+            alert(error?.response?.data?.message || "Signup failed");
+            throw error;
+        }
+    },
+
+  
+
+    logout: async () => {
+        try {
+            const response = await axiosInstance.post('/auth/logout')
+            if (response?.data?.code == 200 || response?.data?.code == 201) {
+                localStorage.removeItem("accessToken");
+                localStorage.removeItem("refreshToken");
+                queryClient.clear();
+                set({ user: null, accessToken: null, refreshToken: null, isAuthenticated: false });
+                return response
+            } else {
+                alert(response?.data?.message)
+            }
+        } catch (error) {
+            alert(error)
+        }
+    },
+
+    resendOtp: (number) => {
+        try {
+            const response = axiosInstance.post(`/auth/resend-otp?mobileNumber=${number}`,)
+            return response
+        } catch (error) {
+            alert(error)
+        }
+    }
+}))
+
+export default useAuthStore;
+
+// 500568683
+// Yaseen@123
